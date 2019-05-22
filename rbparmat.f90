c2f2d58a5a04061ec5e4e1759a4560c5b30fad60
Davide Gatti <davide.gatti@kit.edu>
2019-05-22 08:41:13 +0200

    Corrected typo in MPI_Request

--- rbparmat.f90
+++ rbparmat.f90
@@ -20,14 +20,14 @@ SUBROUTINE LU5decompStep(A)
   integer(C_INT) :: HI1,HI2
   real(C_DOUBLE) :: piv
   real(C_DOUBLE) :: buf(0:1,-2:2)
-  TYPE(MPI_REQUEST) :: Rs,Rr
+  TYPE(MPI_REQUEST) :: R
   TYPE(MPI_STATUS) :: S
   HI1=SIZE(A,1)-1; HI2=SIZE(A,2)-3;
   IF (last) THEN 
     A(HI1-2,1:2)=0; A(HI1-3,2)=0
   ELSE 
-    CALL MPI_IRecv(buf,10,MPI_DOUBLE_PRECISION,ipy+1,TAG_LUDECOMP,MPI_COMM_Y,Rr)
-    CALL MPI_Wait(Rr,S)
+    CALL MPI_IRecv(buf,10,MPI_DOUBLE_PRECISION,ipy+1,TAG_LUDECOMP,MPI_COMM_Y,R)
+    CALL MPI_Wait(R,S)
     A(HI1-1:HI1,:)=buf
   END IF
   DO i=HI1-HI2,0,-1 
@@ -43,8 +43,8 @@ SUBROUTINE LU5decompStep(A)
     A(0,-2:-1)=0; A(1,-2)=0
   ELSE
     buf=A(0:1,:)
-    CALL MPI_ISend(buf,10,MPI_DOUBLE_PRECISION,ipy-1,TAG_LUDECOMP,MPI_COMM_Y,Rs)
-    CALL MPI_Wait(Rr,S)
+    CALL MPI_ISend(buf,10,MPI_DOUBLE_PRECISION,ipy-1,TAG_LUDECOMP,MPI_COMM_Y,R)
+    CALL MPI_Wait(R,S)
   END IF
 END SUBROUTINE LU5decompStep
 !-----------------------------------------------------!
@@ -57,20 +57,20 @@ SUBROUTINE LeftLU5divStep1(x,A,b)
   complex(C_DOUBLE_COMPLEX), intent(out) :: x(-2:)
   real(C_DOUBLE), intent(in)  :: A(0:,-2:)
   integer(C_INT) :: HI1,HI2
-  TYPE(MPI_REQUEST) :: Rs,Rr
+  TYPE(MPI_REQUEST) :: R
   TYPE(MPI_STATUS) :: S
   HI1=SIZE(A,1)-1; HI2=SIZE(A,2)-3;
   x=b
   IF (.NOT. last) THEN 
-    CALL MPI_IRecv(x(HI1-1:HI1),2,cmpl,ipy+1,TAG_LUDIVSTEP1,MPI_COMM_Y,Rr)
-    CALL MPI_Wait(Rr,S)
+    CALL MPI_IRecv(x(HI1-1:HI1),2,cmpl,ipy+1,TAG_LUDIVSTEP1,MPI_COMM_Y,R)
+    CALL MPI_Wait(R,S)
   END IF
   DO i=HI1-HI2,0,-1 
     x(i)=(x(i)-sum(A(i,1:2)*x(i+1:i+2)))*A(i,0)
   END DO
   IF (.NOT. first) THEN 
-    CALL MPI_ISend(x(0:1),2,cmpl,ipy-1,TAG_LUDIVSTEP1,MPI_COMM_Y,Rs)
-    CALL MPI_Wait(Rr,S)
+    CALL MPI_ISend(x(0:1),2,cmpl,ipy-1,TAG_LUDIVSTEP1,MPI_COMM_Y,R)
+    CALL MPI_Wait(R,S)
   END IF
 END SUBROUTINE LeftLU5divStep1
 !-----------------------------------------------------!
@@ -81,19 +81,19 @@ SUBROUTINE LeftLU5divStep2(A,b)
   complex(C_DOUBLE_COMPLEX), intent(inout) :: b(-2:)
   real(C_DOUBLE), intent(in)  :: A(0:,-2:)
   integer(C_INT) :: HI1,HI2
-  TYPE(MPI_REQUEST) :: Rs,Rr
+  TYPE(MPI_REQUEST) :: R
   TYPE(MPI_STATUS) :: S
   HI1=SIZE(A,1)-1; HI2=SIZE(A,2)-3;
   IF (.NOT. first) THEN 
-    CALL MPI_IRecv(b(-2:-1),2,cmpl,ipy-1,TAG_LUDIVSTEP2,MPI_COMM_Y,Rr)
-    CALL MPI_Wait(Rr,S)
+    CALL MPI_IRecv(b(-2:-1),2,cmpl,ipy-1,TAG_LUDIVSTEP2,MPI_COMM_Y,R)
+    CALL MPI_Wait(R,S)
   END IF
   DO i=0,HI1
     b(i)=b(i)-sum(A(i,-2:-1)*b(i-2:i-1))
   END DO
   IF (.NOT. last) THEN 
-    CALL MPI_ISend(b(HI1-3:HI1-2),2,cmpl,ipy+1,TAG_LUDIVSTEP2,MPI_COMM_Y,Rs)
-    CALL MPI_Wait(Rr,S)
+    CALL MPI_ISend(b(HI1-3:HI1-2),2,cmpl,ipy+1,TAG_LUDIVSTEP2,MPI_COMM_Y,R)
+    CALL MPI_Wait(R,S)
   END IF
 END SUBROUTINE LeftLU5divStep2
 !-----------------------------------------------------!
