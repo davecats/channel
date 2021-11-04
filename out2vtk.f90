9264ef82291087ac3a0b5f54e58a9c79e88031d3
andyandreolli <andreavalerio.andreolli@gmail.com>
2021-11-04 13:12:28 +0100

    Fixed FFT in out2vtk; fixed spatial resolution and interpolation.

--- out2vtk.f90
+++ out2vtk.f90
@@ -64,15 +64,22 @@ type(MPI_Datatype) :: wtype_3d, type_towrite ! , wtype_scalar
     call MPI_COMM_SIZE(MPI_COMM_WORLD,nproc,ierr)
     call read_dnsin()
 
+    !-----------!
+    ! ATTENTION !
+    !-----------!
     ! set npy to total number of processes
     ! this effectively DEACTIVATES PARALLELISATION IN Z
     ! and makes life easier (without significant losses in perf.)
     npy = nproc
     ! notice that this overrides value from dns.in
+    !-----------------------------------------------------------------!
+    ! NEVER RUN THIS PROGRAM WITH X/Z PARALLELISATION (it won't work) !
+    !-----------------------------------------------------------------!
 
     call init_MPI(nx+1,nz,ny,nxd+1,nzd)
     call init_memory(.FALSE.)
-    call init_fft(VVdz,VVdx,rVVdx,nxB,nxB,2*nz+1,2*nz+1)
+    call init_fft(VVdz,VVdx,rVVdx,nxB,nxB,2*nz+1,2*nz+1,.TRUE.)
+    ! LAST FLAG OF init_fft (.TRUE.) SPECIFIES THAT REAL FFT TRANSFORM HAS AN ODD LOGICAL SIZE
     ! Notice that in the call to init_fft the values of nxd and nzd have been replaced by nxB and 2*nz+1.
     ! Since the parallelisation in xz is deactivated, nxB=nx+1.
     ! Instead, nxd and nzd are the expanded number of points that are needed for dealiasing;
@@ -171,8 +178,8 @@ type(MPI_Datatype) :: wtype_3d, type_towrite ! , wtype_scalar
         ! WARNING: this differs from normal program, since there is no extension of the Fourier domain
         ! (or, no increase of spatial resolution to avoid dealiasing)
         ! i.e., no need to set anything zo zero in the arrays
-        VVdz(1:nz+1,1:nxB,1:3,1)=V(iy,0:nz,nx0:nxN,1:3);
-        VVdz(nz+2:2*nz+1,1:nxB,1:3,1)=V(iy,-nz:-1,nx0:nxN,1:3); 
+        VVdz(1:nz+1,:,1:3,1)=V(iy,0:nz,:,1:3);
+        VVdz(nz+2:2*nz+1,:,1:3,1)=V(iy,-nz:-1,:,1:3); 
         do iV=1,3
             call IFT(VVdz(1:2*nz+1,1:nxB,iV,1)) ! first transform in z
             
@@ -187,7 +194,7 @@ type(MPI_Datatype) :: wtype_3d, type_towrite ! , wtype_scalar
             end do
             
             call RFT(VVdx(:,:,iV,1),rVVdx(:,:,iV,1)) ! second transform in x
-            ! TODO FIXME this might actually be rVVdx(1:2*nx+2) but idk
+            
         end do
         ! rVVdx is now containing the antitransform
         ! it has size 2*nx+1,2*nz+1,6,6
@@ -205,9 +212,9 @@ type(MPI_Datatype) :: wtype_3d, type_towrite ! , wtype_scalar
         do iz=1,nztot
             do ix=1,nxtot
                 call undersampled_to_fullindex(ix,iz, xfull_idx,zfull_idx)
-                xyz(1,ix,iy,iz) = (xfull_idx-1) * (2 * PI / alfa0)/(2*nx)
+                xyz(1,ix,iy,iz) = (xfull_idx-1) * (2 * PI / alfa0)/(2*nx+1)
                 xyz(2,ix,iy,iz) = y(iy)
-                xyz(3,ix,iy,iz) = (zfull_idx-1) * (2 * PI / beta0)/(2*nz)
+                xyz(3,ix,iy,iz) = (zfull_idx-1) * (2 * PI / beta0)/(2*nz+1)
             end do
         end do
 
@@ -396,12 +403,16 @@ contains !----------------------------------------------------------------------
     ! whereas xprj=1,2*nx+1 and zprj=1,2*nz+1.
     ! However, xprj and zprj are REAL NUMBERS, meaning that (xx,zz) can correspond to a position that is
     ! inbetween two points of the array rVVdx.
+    ! The logic here is that:
+    ! - xx=1 gets mapped to xprj=1 (as this is x=0 in space, i.e. the first extreme of the periodic domain)
+    ! - xx=nxtot+1 gets mapped to xprj=2*nx+2 (as this would be the other extreme of the periodic domain)
+    ! Same applies for z.
 
     integer, intent(in) :: xx, zz
     real, intent(out) :: xprj,zprj
 
-        xprj = (xx - 1.0) / (nxtot-1.0) * (2*nx) + 1
-        zprj = (zz - 1.0) / (nztot-1.0) * (2*nz) + 1
+        xprj = (2*nx+1)/(nxtot) * (xx - 1.0) + 1
+        zprj = (2*nz+1)/(nztot) * (zz - 1.0) + 1
 
     end subroutine undersampled_to_fullindex
 
