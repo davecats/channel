baaffbdfdd3eb9f7525009ceb9399346c941de11
andyandreolli <andreavalerio.andreolli@gmail.com>
2021-11-16 22:22:05 +0100

    Tested FT

--- postpro/am/camstar.f90
+++ postpro/am/camstar.f90
@@ -1,42 +1,3 @@
-
-!--------------------------------------------------!
-!-----------------     LEGEND     -----------------!
-!--------------------------------------------------!
-
-! mean(iv, iy)      where iv        U   W   Uy  Wy  Uyy Wyy P
-!                                   1   2   3   4   5   6   7
-
-! irs ----> index used for Reynolds stress
-!   uu  vv  ww  uv  vw  uw
-!   1   2   3   4   5   6
-
-! uiujprofiles(irs, iterm, iy, largesmall)      where irs defined before, while iterm:
-!   var prod    psdiss      ttrsp   tcross  vdiff   pstrain     ptrsp   PHIttrsp    PHIvdiff    PHIptrsp
-!   1   2       3           4       5       6       7           8       9           10          11
-
-! *VVd*(zf(iz), xf(ix), term, inout)
-! : WARNING :  iz, ix indices here is scrambled as required by FFT -> use zf(iz), xf(ix)
-! - inout = 1:5
-! - term = 1:6
-! inout=1 contains "inputs" (meaning, everything that is necessary for Fourier antitransform in order to go
-! back to real domain). Products are calculated in real domain, and then transformed back to Fourier.
-! All the "outputs" (everything after the Fourier antitransform, so basically products both in real and
-! Fourier domains) are stored in inout=2.
-! As for the "inputs" (inout=1):
-! - term=1:4 are i,j velocity for large and small fields (both for ttrsp and tcross);
-!   please use placeholders i_ft, j_ft summed to ilasm=1:2
-! - term=6 is component k of the whole velocity field - not decomposed (only for tcross);
-!   use placeholder uk_ft
-! - term=5 is not used
-! As for the "outputs" (inout=2):
-! - for ttrsp, term=1:2 are large/small ui*uj product; use index ilasm
-! - for tcross, term=1:4 are ui*uk, uj*uk for large/small;
-!   please use placeholders i_ft, j_ft summed to ilasm=1:2
-!   notice that two terms appear in tcross; for b=2, a=1:4,
-!   the one starting with ui goes under i_fft, the one starting with uj goes under j_fft
-
-
-
 program camstar
 use dnsdata
 implicit none
@@ -55,19 +16,24 @@ logical :: custom_mean = .FALSE.
 ! counters
 
 integer :: ii ! counter initially used for parsing arguments
-integer :: ix, iy, iz, iv ! for spatial directions and components
+integer :: ix, iy, iz, iv, iy1, iy2 ! for spatial directions and components
 
 ! global stuff
 
 integer, parameter :: file_vel = 883
 character(len=40) :: istring, currfname
 
-real(C_DOUBLE), allocatable :: mean(:,:)
+integer :: nyh
+
+real(C_DOUBLE), allocatable :: mean(:,:), CAM(:,:,:,:), CAMi(:,:,:,:), CAM0(:,:,:,:), mean_us2(:,:,:), mean_30(:,:,:), mean_ul(:,:,:), totmean_us2(:,:), totmean_us2_0(:,:)
 
 ! camstar stuff
 
 real :: retau, Lc, Lt, a0, acut
 integer :: icut
+
+integer :: pm
+integer, parameter :: d1=1, u1=2, d2=3, u2=4, large=1, small=2
 
 ! MPI stuff
 
@@ -95,18 +61,27 @@ integer :: ierror
     ! setup
     call read_dnsin(.TRUE.) ! passing .TRUE. cause dns.in is in parent folder wrt cwd
     call camstar_setup()
-    ! set npy to total number of processes
-    ! this effectively DEACTIVATES PARALLELISATION IN X/Z
+    ! set npy to 1
+    ! this effectively DEACTIVATES PARALLELISATION IN Y
     ! and makes life easier (without significant losses in perf.)
-    npy = nproc
+    npy = 1
     ! notice that this overrides value from dns.in
     call init_MPI(nx+1,nz,ny,nxd+1,nzd)
     call init_memory(.FALSE.) ! false flag avoids allocation of RHS related stuff!
-    call init_fft(VVdz,VVdx,rVVdx,nxd,nxB,nzd,nzB,.TRUE.) ! LAST FLAG (.TRUE.) SPECIFIES THAT REAL FFT TRANSFORM HAS AN ODD LOGICAL SIZE
+    call init_fft(VVdz,VVdx,rVVdx,nxd,nxB,nzd,nzB,.FALSE.,[4,2]) ! size of fft array is specified with optional flag
 
     ! allocate stuff
     call init_camstar_mpitypes()
     allocate(mean(1:2, ny0-2:nyN+2))
+    nyh = ny/2 ! watch out: this is an integer division!
+    allocate(CAM(1:3, 0:2, 0:nyh, 0:nyh))
+    allocate(CAMi(1:3, 0:2, 0:nyh, 0:nyh))
+    allocate(CAM0(1:3, 0:2, 0:nyh, 0:nyh))
+    allocate(totmean_us2(0:nyh, 0:nyh))
+    allocate(totmean_us2_0(0:nyh, 0:nyh))
+    allocate(mean_us2(1:2, 0:nyh, 0:nyh))
+    allocate(mean_ul(1:2, 0:nyh, 0:nyh))
+    allocate(mean_30(1:2, 0:nyh, 0:nyh))
 
     !---------------------------------------------------!
     !----------------- COMPUTE AVERAGE -----------------!
@@ -134,8 +109,114 @@ integer :: ierror
     !------------------------------------------------!
     !--------------- COMPUTE CAMSTAR ----------------!
     !------------------------------------------------!
-
     
+    if (has_terminal) print *, "Computing CAM..."
+
+    ! set to zero arrays of interest
+    CAM = 0 ! not necessary with CAMi: done for every field
+    totmean_us2 = 0
+    mean_us2 = 0
+    mean_ul = 0
+
+    ! open file for writing CAMi
+    if (has_terminal) then
+        currfname = "CAMstari.bin"
+        open(unit=100,file=TRIM(currfname),access="stream",action="write")
+    end if
+    
+    do ii = nfmin, nfmax, dnf ! loop over files
+
+        ! read velocity
+        write(istring,*) ii
+        currfname = trim("../Dati.cart."//TRIM(ADJUSTL(istring))//".out")
+        call read_restart_file(currfname, V)
+
+        ! remove mean
+        if (has_average) then
+            V(:,0,0,1) = V(:,0,0,1) - cmplx(mean(1,:))
+            V(:,0,0,3) = V(:,0,0,3) - cmplx(mean(2,:))
+        end if
+
+        CAMi = 0
+
+        do iy2 = 0,nyh 
+            do iy1 = 0,nyh
+
+                ! do filtering at the 4 points of interest (y1 and y2 in both top and bottom halves of plane)
+                call scale_filter(iy1, iy2)
+                ! result of filtering is in rVVdx; see comments in subroutine scale_filter
+                
+                do iz = 1, nzB ! only own block in z
+                    do ix = 1, (2*nxd) ! all points in x
+
+                        CAMi(1,0,iy1,iy2) = CAMi(1,0,iy1,iy2) + (rVVdx(ix,iz,d1,small)**2)*rVVdx(ix,iz,d2,large) + (rVVdx(ix,iz,u1,small)**2)*rVVdx(ix,iz,u2,large)
+                        CAMi(2,0,iy1,iy2) = CAMi(2,0,iy1,iy2) + (rVVdx(ix,iz,d1,small)**2)*(rVVdx(ix,iz,d2,small)**2) + (rVVdx(ix,iz,u1,small)**2)*(rVVdx(ix,iz,u2,small)**2)
+                        CAMi(3,0,iy1,iy2) = CAMi(3,0,iy1,iy2) + rVVdx(ix,iz,d1,large)*rVVdx(ix,iz,d2,large) + rVVdx(ix,iz,u1,large)*rVVdx(ix,iz,u2,large)
+                        totmean_us2(iy1,iy2) = totmean_us2(iy1,iy2) + rVVdx(ix,iz,d1,small) + rVVdx(ix,iz,u1,small)
+                        
+                        ! bottom wall, conditional
+                        if (rVVdx(ix,iz,d2,large) > 0) then 
+                            pm = 1
+                        else
+                            pm = 2
+                        end if
+                        CAMi(1,pm,iy1,iy2) = CAMi(1,pm,iy1,iy2) + (rVVdx(ix,iz,d1,small)**2)*rVVdx(ix,iz,d2,large)
+                        CAMi(2,pm,iy1,iy2) = CAMi(2,pm,iy1,iy2) + (rVVdx(ix,iz,d1,small)**2)*(rVVdx(ix,iz,d2,small)**2)
+                        CAMi(3,pm,iy1,iy2) = CAMi(3,pm,iy1,iy2) + rVVdx(ix,iz,d1,large)*rVVdx(ix,iz,d2,large)
+                        mean_us2(pm,iy1,iy2) = mean_us2(pm,iy1,iy2) + (rVVdx(ix,iz,d1,small)**2)
+                        mean_ul(pm,iy1,iy2) = mean_ul(pm,iy1,iy2) + rVVdx(ix,iz,d2,large)
+                        
+                        ! top wall, conditional
+                        if (rVVdx(ix,iz,u2,large) > 0) then 
+                            pm = 1
+                        else
+                            pm = 2
+                        end if
+                        CAMi(1,pm,iy1,iy2) = CAMi(1,pm,iy1,iy2) + (rVVdx(ix,iz,u1,small)**2)*rVVdx(ix,iz,u2,large)
+                        CAMi(2,pm,iy1,iy2) = CAMi(2,pm,iy1,iy2) + (rVVdx(ix,iz,u1,small)**2)*(rVVdx(ix,iz,u2,small)**2)
+                        CAMi(3,pm,iy1,iy2) = CAMi(3,pm,iy1,iy2) + rVVdx(ix,iz,u1,large)*rVVdx(ix,iz,u2,large)
+                        mean_us2(pm,iy1,iy2) = mean_us2(pm,iy1,iy2) + (rVVdx(ix,iz,u1,small)**2)
+                        mean_ul(pm,iy1,iy2) = mean_ul(pm,iy1,iy2) + rVVdx(ix,iz,u2,large)
+
+                    end do
+                end do                
+
+            end do
+        end do
+
+        ! sum results of all processes into process 0
+        ! call MPI_REDUCE(CAMi, CAM0, 9*(nyh+1)*(nyh+1), MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+        ! call MPI_REDUCE(mean_us2, mean_30, 3*(nyh+1)*(nyh+1), MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+        ! if (has_terminal) mean_us2 = mean_30
+        ! call MPI_REDUCE(mean_ul, mean_30, 3*(nyh+1)*(nyh+1), MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+        ! if (has_terminal) mean_ul = mean_30
+        ! call MPI_REDUCE(totmean_us2, totmean_us2_0, (nyh+1)*(nyh+1), MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+
+        if (has_terminal) then
+
+            ! divide by number of points in x and z (* 2 due to both half channels)
+            CAM0 = CAM0 / (2*(2*nxd)*nzd)
+
+            ! write camstari
+            write(100) CAM0
+
+            ! cumulate CAM0 in CAM
+            CAM = CAM + CAM0/nftot
+
+        end if 
+
+    end do
+
+    ! close file where CAMi was written
+    if (has_terminal) close(100)
+
+    ! apply correction for conditional averages
+    mean_ul = mean_ul / ( (2*2*nxd*nzd) * nftot )
+    mean_us2 = mean_us2 / ( (2*2*nxd*nzd) * nftot )
+    totmean_us2_0 = totmean_us2_0 / ( (2*2*nxd*nzd) * nftot )
+    do ii=1,2
+        CAM(1,ii,:,:) = CAM(1,ii,:,:) - (mean_ul(ii,:,:) * totmean_us2_0(:,:))
+    end do
 
     !---------------------------------------------------!
     !-----------------      WRITE      -----------------!
@@ -149,6 +230,14 @@ integer :: ierror
         CALL MPI_File_set_view(fh, 0_MPI_OFFSET_KIND, MPI_DOUBLE_PRECISION, mean_write_type, 'native', MPI_INFO_NULL)
         CALL MPI_File_write_all(fh, mean, 1, mean_inmem_type, MPI_STATUS_IGNORE)
     call MPI_File_close(fh)
+
+    ! write CAMstar
+    if (has_terminal) then
+        currfname = "CAMstari.bin"
+        open(unit=100,file=TRIM(currfname),access="stream",action="write")
+            write(100) CAM
+        close(100)
+    end if
 
     ! be polite and say goodbye
     if (has_terminal) print *, "Goodbye man!"
@@ -180,16 +269,109 @@ contains !----------------------------------------------------------------------
         Lt=retau*(2*PI/beta0); a0=2*PI/Lt; acut=2*PI/Lc; icut=FLOOR(acut/a0)
 
         ! write CAMstart.nfo
-        if (has_terminal) then
+        if (iproc==0) then
             open(15, file='CAMstart.nfo')
                 write(15,*) "This is camstar.f90."
-                write(15,*) "nmin=", nmin, " nmax=", nmax, " dn=", dn, " nftot=", nftot
-                write(15,*) "nmin_cm=", nmin_cm, " nmax_cm=", nmax_cm, " dn_cm=", dn_cm
-                write(15,*) "Lt=", Lt, " Lc=", Lc, " icut=", icut, " 1000*2*PI/(beta0*icut)=", 1000*2*PI/(beta0*icut)
+                write(15,*) ""
+                write(15,*) "nmin", nmin 
+                write(15,*) "nmax", nmax
+                write(15,*) "dn", dn
+                write(15,*) "nftot", nftot
+                write(15,*) ""
+                write(15,*) "nmin_cm", nmin_cm
+                write(15,*) "nmax_cm", nmax_cm
+                write(15,*) "dn_cm", dn_cm
+                write(15,*) "" 
+                write(15,*) "Lt", Lt
+                write(15,*) "Lc", Lc
+                write(15,*) "icut", icut
+                write(15,*) "1000*2*PI/(beta0*icut)", 1000*2*PI/(beta0*icut)
             close(15)
         end if
 
     end subroutine camstar_setup
+
+
+
+    subroutine scale_filter(iy1, iy2)
+    ! after this subroutine is called, rVVdx will contain the antitransforms
+    ! of the u-field (1st component) in a xz plane at different positions, divided in
+    ! large and small contributions. The structure of rVVdx is:
+    ! rVVdx has size 2*(nxd+1),nzB,4,2
+    ! 1. x-index: you actually need the first 2*nzd points, the last 3 are always 0
+    ! 2. z-index
+    ! 3. these points refer to different positions in y; use the parameters d1,u1,d2,u2,
+    !    which correspond to y indeces y1, get_up(y1), y2, get_up(y2)
+    ! 4. large or small field; see parameters large and small
+
+        integer, intent(in) :: iy1, iy2
+        integer :: up1, up2, icnt
+
+        up1=get_up(iy1); up2=get_up(iy2) ! retrieves index of corresponding y location in upper half of channel
+
+        ! small: icut+1:nz   ---   large: 0:icut
+
+        ! copy V to VVdz array for FFT
+        call copy4fft(d1,iy1,small); call copy4fft(u1,up1,small);  call copy4fft(d2,iy2,small); call copy4fft(u2,up2,small)
+        call copy4fft(d1,iy1,large); call copy4fft(u1,up1,large);  call copy4fft(d2,iy2,large); call copy4fft(u2,up2,large)
+
+        ! do fourier transform
+        do icnt=1,2
+            do iV=1,4
+                call IFT(VVdz(:,:,iV,icnt)) ! first transform in z
+                call MPI_Alltoall(VVdz(:,:,iv,icnt), 1, Mdz, VVdx(:,:,iv,icnt), 1, Mdx, MPI_COMM_X)
+                VVdx(nx+2:nxd+1,1:nzB,icnt,iV)=0
+                call RFT(VVdx(:,:,iV,icnt),rVVdx(:,:,iV,icnt)) ! second transform in x
+            end do
+        end do
+
+    end subroutine
+
+
+
+    subroutine copy4fft(iw,ir,lasm)
+    integer, intent(in) :: iw, ir, lasm
+    integer :: iwp1, iwp2, iwm1, iwm2, irp1, irp2, irm1, irm2
+
+        ! WARNING: this differs from normal program, since there is no extension of the Fourier domain
+        ! (or, no increase of spatial resolution to avoid dealiasing)
+        ! i.e., no need to set anything zo zero in the arrays
+
+        if (lasm == small) then
+
+            ! iz >= 0: iz_fft = iz + 1
+            iwp1 = (icut+2);                irp1 = (icut+1)
+            iwp2 = (nz+1);                  irp2 = nz
+
+            ! iz < 0: iz_fft = 2*(nz+1) + iz
+            iwm1 = (nzd+1-nz);              irm1 = -nz
+            iwm2 = (nzd+1-icut-1);          irm2 = (-icut-1)
+
+        else
+
+            ! iz >= 0: iz_fft = iz + 1
+            iwp1 = 1;                       irp1 = 0
+            iwp2 = (icut+1);                irp2 = icut
+
+            ! iz < 0: iz_fft = 2*(nz+1) + iz
+            iwm1 = (nzd+1-icut);            irm1 = (-icut)
+            iwm2 = (nzd+1-1);               irm2 = (-1)
+
+        end if
+
+        ! actual copying
+        VVdz(:,:,iw,lasm) = 0
+        VVdz(iwp1:iwp2, :, iw, lasm) = V(ir, irp1:irp2, :, 1) ! iz >= 0
+        VVdz(iwm1:iwm2, :, iw, lasm) = V(ir, irm1:irm2, :, 1) ! iz <= 0
+    end subroutine
+
+
+
+    function get_up(iy) result(iyup)
+    integer, intent(in) :: iy
+    integer :: iyup
+        iyup = ny-iy
+    end function
 
 
 
@@ -260,6 +442,7 @@ contains !----------------------------------------------------------------------
 
     ! read arguments
     subroutine parse_args()
+        integer :: ii
         ii = 1
         do while (ii <= command_argument_count()) ! parse optional arguments
             call get_command_argument(ii, arg)
