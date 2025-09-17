2f92bb2ddf2953a42fdb08c7298a44b9f0456fa8
Jonathan Neuhauser <jonathan.neuhauser@kit.edu>
2025-09-17 20:22:28 +0200

    Rewrite MPI comm to use GPU-aware MPI

--- mpi_transpose.f90
+++ mpi_transpose.f90
@@ -23,9 +23,9 @@ MODULE mpi_transpose
 
   TYPE(MPI_Comm) :: MPI_CART_COMM, MPI_COMM_X, MPI_COMM_Y
   integer(C_INT), save :: nproc, iproc, ierr, nzd, nx
-  integer(C_INT), save :: nx0, nxN, nxB, nz0, nzN, nzB, ny0, nyN, miny, maxy, block
+  integer(C_INT), save :: nx0, nxN, nxB, nz0, nzN, nzB, ny0, nyN, miny, maxy
   !$omp declare target(ny0, nyN)
-  complex(C_DOUBLE_COMPLEX), allocatable :: Ain(:), Aout(:)
+  complex(C_DOUBLE_COMPLEX), allocatable :: sendbuf(:), recvbuf(:)
   logical, save :: has_terminal, has_average
   TYPE(MPI_Datatype), save :: writeview_type, owned2write_type, vel_read_type, vel_field_type
 
@@ -34,57 +34,67 @@ CONTAINS
   !-------------- Transpose: Z to X --------------!
   !-----------------------------------------------!
   SUBROUTINE zTOx(Vz, Vx, ny, nPhi)
-    complex(C_DOUBLE_COMPLEX), intent(in)  :: Vz(1:, 1:, :, :)
+    IMPLICIT NONE
+    complex(C_DOUBLE_COMPLEX), intent(inout)  :: Vz(1:, 1:, :, :)
     complex(C_DOUBLE_COMPLEX), intent(out) :: Vx(1:, 1:, :, :)
     integer(C_INT), intent(in) :: ny, nPhi
-    integer(C_SIZE_T) :: i, j, iy, iV
-    integer(C_SIZE_T) :: ix, iz, one, num, zero, npxt
+    integer(C_SIZE_T) :: iy, iV, ix, iz, dest, src
+    integer :: nField
+    integer :: sendcount, p
 
     IF (nproc == 1) THEN
       !$omp target teams distribute parallel do collapse(4) defaultmap(none) default(none) &
-      !$omp shared(Vx, Vz) shared(ny, nx0, nzd, nx, nPhi) private(iy, iV, ix, iz)
+      !$omp shared(Vx, Vz) shared(ny, nzd, nx, nPhi) private(iy, iV, ix, iz)
       DO iy = 1, ny + 3
         DO iV = 1, 3 + nPhi
-          DO ix = 0, nx
+          DO ix = 1, nx + 1
             DO iz = 1, nzd
-              Vx(ix + 1, iz, iy, iV) = Vz(iz, ix - nx0 + 1, iy, iV)
+              Vx(ix, iz, iy, iV) = Vz(iz, ix, iy, iV)
             END DO
           END DO
         END DO
       END DO
 
     ELSE
-      !$omp target update from(Vz)
-      DO iy = 1, ny + 3
-        DO iV = 1, 3 + nPhi
-          one = 1
-          num = nzd/nproc
-          zero = 0
-          npxt = nproc
 
-          Ain = 0; Aout = 0
+      nField = 3 + nPhi        ! same as your iV loop upper bound
+      sendcount = nxB*nzB*(ny + 3)*nField
 
-          !Pack
-          i = zero
-          DO j = zero, npxt - one
-            DO ix = nx0, nxN
-              Ain(i:i + num - one) = Vz(one + j*num:(j + one)*num, ix - nx0 + one, iy, iV)
-              i = i + num
-            END DO
-          END DO
+      !$omp target teams distribute parallel do collapse(5) defaultmap(none) default(none) &
+      !$omp shared(Vz, sendbuf) shared(ny, nxB, nzB, nproc, nField, sendcount) private(iy, iV, ix, iz, dest, p)
+      do dest = 0, nproc - 1
+        do iV = 1, nField
+          do iy = 1, ny + 3
+            do ix = 1, nxB
+              do iz = 1, nzB
+                p = dest*sendcount + iz + (nzB*(ix - 1)) + (nzB*nxB*(iy - 1)) + (nzB*nxB*(ny + 3)*(iV - 1))
+                sendbuf(p) = Vz(dest*nzB + iz, ix, iy, iV)
+              end do
+            end do
+          end do
+        end do
+      end do
 
-          !Send
-          CALL MPI_Alltoall(Ain, nxB*nzB, MPI_DOUBLE_COMPLEX, Aout, nxB*nzB, MPI_DOUBLE_COMPLEX, MPI_COMM_WORLD)
+      !$omp target data use_device_ptr(sendbuf, recvbuf)
+      call MPI_Alltoall(sendbuf, sendcount, MPI_DOUBLE_COMPLEX, &
+                        recvbuf, sendcount, MPI_DOUBLE_COMPLEX, MPI_COMM_WORLD, ierr)
 
-          !Unpack
-          i = zero
-          DO ix = 0, nx
-            Vx(ix + one, one:num, iy, iV) = Aout(i:i + num - one)
-            i = i + num
-          END DO
-        END DO
-      END DO
-      !$omp target update to(Vx)
+      !$omp end target data
+      !$omp target teams distribute parallel do collapse(5) defaultmap(none) default(none) &
+      !$omp shared(Vx, recvbuf) shared(ny, nxB, nzB, nproc, nField, sendcount) private(iy, iV, ix, iz, dest, p)
+      do src = 0, nproc - 1
+        do iV = 1, nField
+          do iy = 1, ny + 3
+            do ix = 1, nxB
+              do iz = 1, nzB
+                p = src*sendcount + iz + (nzB*(ix - 1)) + (nzB*nxB*(iy - 1)) + (nzB*nxB*(ny + 3)*(iV - 1))
+                Vx(ix + src*nxB, iz, iy, iV) = recvbuf(p)
+              end do
+            end do
+          end do
+        end do
+      end do
+
     END IF
 
   END SUBROUTINE zTOx
@@ -92,56 +102,70 @@ CONTAINS
   !-------------- Transpose: X to Z --------------!
   !-----------------------------------------------!
   SUBROUTINE xTOz(Vx, Vz, ny, nPhi)
-    complex(C_DOUBLE_COMPLEX), intent(out) :: Vz(1:, 1:, :, :)
-    complex(C_DOUBLE_COMPLEX), intent(in) :: Vx(1:, 1:, :, :)
-    integer(C_INT), intent(in) :: ny, nPhi
-    integer(C_SIZE_T) :: i, j, iy, iV
-    integer(C_SIZE_T) :: ix, iz, one, num, zero, npxt
+    use iso_c_binding, only: C_INT, C_SIZE_T, C_DOUBLE_COMPLEX
+    implicit none
 
+    complex(C_DOUBLE_COMPLEX), intent(out) :: Vz(1:, 1:, :, :)
+    complex(C_DOUBLE_COMPLEX), intent(in)  :: Vx(1:, 1:, :, :)
+    integer(C_INT), intent(in) :: ny, nPhi
+    integer(C_SIZE_T) :: iy, iV, ix, iz, dest, src
+    integer :: nField
+    integer :: sendcount, p
+
+    ! --- single-rank short-circuit ---
     IF (nproc == 1) THEN
       !$omp target teams distribute parallel do collapse(4) defaultmap(none) default(none) &
-      !$omp shared(Vx, Vz) shared(ny, nz0, nzd, nx, nPhi) private(iy, iV, ix, iz)
+      !$omp shared(Vx, Vz) shared(ny, nzd, nx, nPhi) private(iy, iV, ix, iz)
       DO iy = 1, ny + 3
         DO iV = 1, 6 + 3*nPhi
-          DO iz = 0, nzd - 1
+          DO iz = 1, nzd
             DO ix = 1, nx + 1
-              Vz(iz + 1, ix, iy, iV) = Vx(ix, iz - nz0 + 1, iy, iV)
+              Vz(iz, ix, iy, iV) = Vx(ix, iz, iy, iV)
             END DO
           END DO
         END DO
       END DO
+
     ELSE
+      nField = 6 + 3*nPhi
+      sendcount = nxB*nzB*(ny + 3)*nField
 
-      !$omp target update from (Vx)
-      DO iy = 1, ny + 3
-        DO iV = 1, 6 + 3*nPhi
-          one = 1
-          num = (nx + 1)/nproc
-          zero = 0
-          npxt = nproc
+      !$omp target teams distribute parallel do collapse(5) defaultmap(none) default(none) &
+      !$omp shared(Vx, sendbuf) shared(ny, nxB, nzB, nproc, nField, sendcount) private(iy, iV, ix, iz, dest, p)
+      do dest = 0, nproc - 1
+        do iV = 1, nField
+          do iy = 1, ny + 3
+            do iz = 1, nzB
+              do ix = 1, nxB
+                p = dest*sendcount + ix + (nxB*(iz - 1)) + (nxB*nzB*(iy - 1)) + (nxB*nzB*(ny + 3)*(iV - 1))
+                sendbuf(p) = Vx(dest*nxB + ix, iz, iy, iV)
+              end do
+            end do
+          end do
+        end do
+      end do
 
-          !Pack
-          i = zero
-          DO j = zero, npxt - one
-            DO iz = nz0, nzN
-              Ain(i:i + num - one) = Vx(one + j*num:(j + one)*num, iz - nz0 + one, iy, iV)
-              i = i + num
-            END DO
-          END DO
+      !$omp target data use_device_ptr(sendbuf, recvbuf)
+      call MPI_Alltoall(sendbuf, sendcount, MPI_DOUBLE_COMPLEX, &
+                        recvbuf, sendcount, MPI_DOUBLE_COMPLEX, MPI_COMM_WORLD, ierr)
+      !$omp end target data
 
-          !Send
-          CALL MPI_Alltoall(Ain, nxB*nzB, MPI_DOUBLE_COMPLEX, Aout, nxB*nzB, MPI_DOUBLE_COMPLEX, MPI_COMM_WORLD)
-
-          !Unpack
-          i = zero
-          DO iz = 0, nzd - 1
-            Vz(iz + one, one:num, iy, iV) = Aout(i:i + num - one)
-            i = i + num
-          END DO
-        END DO
-      END DO
-      !$omp target update to(Vz)
+      !$omp target teams distribute parallel do collapse(5) defaultmap(none) default(none) &
+      !$omp shared(Vz, recvbuf) shared(ny, nxB, nzB, nproc, nField, sendcount) private(iy, iV, ix, iz, dest, p)
+      do src = 0, nproc - 1
+        do iV = 1, nField
+          do iy = 1, ny + 3
+            do iz = 1, nzB
+              do ix = 1, nxB
+                p = src*sendcount + ix + (nxB*(iz - 1)) + (nxB*nzB*(iy - 1)) + (nxB*nzB*(ny + 3)*(iV - 1))
+                Vz(iz + src*nzB, ix, iy, iV) = recvbuf(p)
+              end do
+            end do
+          end do
+        end do
+      end do
     END IF
+
   END SUBROUTINE xTOz
 
   !------- Divide the problem in 1D slices -------!
@@ -160,7 +184,6 @@ CONTAINS
     ! Calculate domain division
     nx0 = iproc*(nxpp)/nproc; nxN = (iproc + 1)*(nxpp)/nproc - 1; nxB = nxN - nx0 + 1; 
     nz0 = iproc*nzd/nproc; nzN = (iproc + 1)*nzd/nproc - 1; nzB = nzN - nz0 + 1; 
-    block = max(nxB*nzd, nxpp*nzB)
     has_average = (nx0 == 0)
 #ifdef mpiverbose
     DO i = 0, nproc - 1
@@ -169,9 +192,17 @@ CONTAINS
     END DO
     FLUSH (output_unit)
 #endif
+    if (int(nproc, 8)*int(nxB, 8)*int(nzB, 8)*int(ny + 3, 8)*int(6 + 3*nPhi, 8) > huge(0_C_INT)) then
+      if (has_terminal) then
+        print *, "Error: problem too large for MPI transpose (integer overflow). Try to increase the number of processes."
+      end if
+      CALL MPI_Abort(MPI_COMM_WORLD, 1, ierror)
+    end if
+
     ! Allocate buffers for transposes
-    ALLOCATE (Ain(0:block - 1)); Ain = 0
-    ALLOCATE (Aout(0:block - 1)); Aout = 0
+    ALLOCATE (sendbuf(nproc*nxB*nzB*(ny + 3)*(6 + 3*nPhi))); sendbuf = 0
+    ALLOCATE (recvbuf(nproc*nxB*nzB*(ny + 3)*(6 + 3*nPhi))); recvbuf = 0
+    !$omp target enter data map(alloc: sendbuf, recvbuf)
     ! For READING VELOCITY, SETTING VIEW: datatype that maps velocity on disk to memory (it differs from writing: halo cells are read twice!)
     CALL MPI_Type_create_subarray(ndims, [ny+3, 2*nz+1, nxpp, 3+nPhi], [nyN-ny0+5, 2*nz+1, nxB, 3+nPhi], [ny0-1,0,nx0,0], MPI_ORDER_FORTRAN, MPI_DOUBLE_COMPLEX, vel_read_type, ierror)
     CALL MPI_Type_commit(vel_read_type, ierror)
