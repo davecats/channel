ed817eddea5dd46d7664fa2acc8a6a95a18e9215
Jonathan Neuhauser <jonathan.neuhauser@kit.edu>
2026-05-24 19:46:56 +0000

    Add xfail test for convection velocity statistics

---
+++ tests/convvelo/generate_cmd.sh
@@ -0,0 +1,16 @@
+# Generate the test file from the python demo
+module load toolkits/nvhpc/25.5 
+mkdir -p tests/convvelo 
+cp tests/data/dns_test_scalar.in tests/convvelo/dns.in 
+cp tests/data/start_field_scalar.out tests/convvelo/Dati.cart.out 
+cd tests/convvelo 
+sed -i 's/^30\s\+-1\s\+7000\s\+\.TRUE\./1 -1 7000 .TRUE./' dns.in 
+sed -i 's/3\s\+! nstep/10   ! nstep/' dns.in
+mpirun -n 2 ../../build/channel
+mpirun -n 1 ../../build/post_pressure
+# Rename output files from *.dat to *.fld
+for file in *.dat; do
+    mv "$file" "${file%.dat}.fld"
+done
+python3 /net/istmrhea/Data/jonaneuh/2026_convvelo/export_scalar_raw_statistics.py .
+rm *.fld
